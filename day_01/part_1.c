b8d432c5992566ce78223652fd9769baa68631f1
Adam Juraszek <adam.juraszek@LTJ7C30J3M.local>
2022-12-01 20:34:53 +0000

    day 01 solution - c

---
+++ day_01/part_1.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int max_int(const int a, const int b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+void process_lines(int *max_calories, int *calories) {
+
+  FILE *file;
+
+  file = fopen("./input.txt", "r");
+  size_t read;
+  char *line = NULL;
+  size_t line_len = 0;
+
+  while ((read = getline(&line, &line_len, file)) != -1) {
+    int line_val = atoi(line);
+    *calories += line_val;
+    if (line_val == 0) {
+      *max_calories = max_int(*max_calories, *calories);
+      *calories = 0;
+    }
+  }
+
+  *max_calories = max_int(*max_calories, *calories);
+  *calories = 0;
+
+
+  fclose(file);
+}
+
+
+int main(int argc, char **argv) {
+
+  int max_calories = 0;
+  int calories = 0;
+  process_lines(&max_calories, &calories);
+  printf("max calories: %d\n", max_calories);
+  return 0;
+}
---
+++ day_01/part_2.c
@@ -0,0 +1,60 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void shift_array_down(int *array, int i) {
+  /*
+   * ([0,1,2,3], 2) -> [1,2,2,3]
+   */
+  for (int j = 0; j < i; j++) {
+    array[j] = array[j + 1];
+  }
+}
+
+void process_elf_calories(int *max_calories, int elf_calories) {
+
+  if (elf_calories >= max_calories[0]) {
+    for (int i = 2; i >= 0; i--) {
+      if (elf_calories >= max_calories[i]) {
+        shift_array_down(max_calories, i);
+        max_calories[i] = elf_calories;
+        break;
+      }
+    }
+  }
+}
+
+void process_lines(int *max_calories) {
+  int calories = 0;
+
+  FILE *file;
+
+  file = fopen("./input.txt", "r");
+  size_t read;
+  char *line = NULL;
+  size_t line_len = 0;
+
+  while ((read = getline(&line, &line_len, file)) != -1) {
+    int line_val = atoi(line);
+    calories += line_val;
+
+    if (line_val == 0) {
+      process_elf_calories(max_calories, calories);
+      calories = 0;
+    }
+  }
+  process_elf_calories(max_calories, calories);
+  calories = 0;
+  fclose(file);
+}
+
+
+int main(int argc, char **argv) {
+  int max_calories[3] = {0, 0, 0};
+
+  process_lines(max_calories);
+  printf("top 3 calories: %d %d %d\n", max_calories[0], max_calories[1], max_calories[2]);
+  printf("sum of top 3 elf calories %d\n", max_calories[0] + max_calories[1] + max_calories[2]);
+  return 0;
+}
