f7d630a2c137a90fca76c74bd57468d2b10a327e
Adam Juraszek <adam.juraszek@LTJ7C30J3M.local>
2022-12-08 01:24:10 +0000

    temp

--- day_07/part1.c
+++ day_07/part1.c
@@ -1,23 +1,10 @@
 #include "../common/str_utils.h"
 #include <stdio.h>
 
-void cb(char *line) {
-
-  if (line[0] == '$' && line[2] == 'c') {
-    char *dirname;
-    parsef_dyn("$ cd %s\n", line, &dirname);
-    printf("cd to %s\n", dirname);
-    return;
-  }
-
-  if (line[0] == '$' && line[2] == 'l') {
-    return;
-  }
-
-  if (line[0] == 'd') {
-    return;
-  }
-}
+typedef struct filesys {
+  int file_sizes;
+  struct filesys *subdirs;
+} filesys;
 
 int main(int argc, char **argv) {
   char input_file[] = "./test_input.txt";
@@ -27,7 +14,34 @@ int main(int argc, char **argv) {
   size_t line_len = 0;
   char *line = NULL;
 
+
   while ((read = getline(&line, &line_len, file)) != -1) {
+    line[strcspn(line, "\n")] = 0;
+
+    if (line[0] == '$' && line[2] == 'c') {
+      char *dirname = line + 5;
+      printf("cd to %s\n", dirname);
+      continue;
+    }
+
+    if (line[0] == '$' && line[2] == 'l') {
+      while ((read = getline(&line, &line_len, file)) != -1 && line[0] != '$')  {
+
+        if (line[0] == 'd') {
+          line[strcspn(line, "\n")] = 0;
+          char *register_dir = line + 4;
+          printf("register dir %s\n", register_dir);
+        } else {
+          int size;
+          char *filename;
+          parsef_dyn("%d %s\n", line, &size, &filename);
+          filename[strcspn(filename, "\n")] = 0;
+          printf("f: %d %s\n", size, filename);
+        }
+      } 
+      continue;
+    }
+
   }
 
   fclose(file);
